14072ec2085cdaf88c56805020ab88756d20bab5
tobiasz golian <tobiasz.golian@travelperk.com>
2022-01-18 19:43:44 +0100

    reviews model migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_183042) do
+ActiveRecord::Schema.define(version: 2022_01_18_184315) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2022_01_18_183042) do
     t.string "phone_number"
     t.string "integer"
     t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
