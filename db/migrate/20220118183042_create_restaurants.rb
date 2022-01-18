08a846708244b7076b8ae743f58ed681f344fbea
tobiasz golian <tobiasz.golian@travelperk.com>
2022-01-18 19:33:16 +0100

    restaurant migration

---
+++ db/migrate/20220118183042_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.string :phone_number
+      t.string :integer
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
